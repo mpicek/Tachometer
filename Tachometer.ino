e3df4863c6c584213c890f8db13cf408702e5e5d
Martin Picek <martinpicek4@gmail.com>
2019-01-27 17:21:06 +0100

    Fixed issues #7 and #6 - adjustable circumference, erasable distance, time and average speed

--- Tachometer.ino
+++ Tachometer.ino
@@ -59,6 +59,13 @@ volatile uint16_t t2 = 0; //turn of 2nd turn of wheel
 volatile uint16_t t3 = 0;
 volatile uint8_t underMinimalSpeed = 0; //indicates, that bike is under minimal speed
 volatile uint32_t todayTime = 0; //whole time of the day (in timer pulses, so: 1sec = 11718.75 ~= 11719)
+volatile int numberOfPressesRight = 0;
+volatile int numberOfPressesLeft = 0;
+volatile int firstLeftPress = 0;
+volatile int firstRightPress = 0;
+volatile int normalMode = 1;
+volatile uint8_t changeWheel[4];
+volatile uint8_t positionSettings = 3;
 
 uint16_t wheelCircumference = 2000; //in mm ... 2136 ... obvod kola
 
@@ -70,7 +77,13 @@ uint32_t countSpeed(){ // returns 1000x bigger speed (but in km/h) .. but only i
         return 0;
     }
 }
-
+void setWheelCircumferenceVariable(){
+    wheelCircumference = 0;
+    wheelCircumference += 1000*EEPROM.read(0);
+    wheelCircumference += 100*EEPROM.read(1);
+    wheelCircumference += 10*EEPROM.read(2);
+    wheelCircumference += EEPROM.read(3);
+}
 void initializeValues(){
     numberOfRotations = 0; //counts turns of the wheel
     t1 = 0; //time of 1st turn of wheel
@@ -78,11 +91,20 @@ void initializeValues(){
     t3 = 0;
     underMinimalSpeed = 0; //indicates, that bike is under minimal speed
     todayTime = 0; //whole time of the day (in timer pulses, so: 1sec = 11718.75 ~= 11719)
+    numberOfPressesRight = 0;
+    numberOfPressesLeft = 0;
+    setWheelCircumferenceVariable();
+    changeWheel[0] = EEPROM.read(0);
+    changeWheel[1] = EEPROM.read(1);
+    changeWheel[2] = EEPROM.read(2);
+    changeWheel[3] = EEPROM.read(3);
+    positionSettings = 3;
 }
 
 void printOnLCD(){
-
     char stringed[11];
+    if(normalMode){
+    
     uint32_t speed = countSpeed();
 
     LcdClear();
@@ -107,6 +129,51 @@ void printOnLCD(){
     GotoXY(0,5);
     secondsToTime(stringed, todayTime/1000);
     LcdString(stringed);
+    }
+    
+    else{ //show changing wheel circumference
+        LcdClear();
+        GotoXY(0,0);
+        LcdString("hello");
+        GotoXY(25, 4);
+        stringed[0] = '0'+ changeWheel[0];
+        stringed[1] = '0'+ changeWheel[1];
+        stringed[2] = '0'+ changeWheel[2];
+        stringed[3] = '0'+ changeWheel[3];
+        stringed[4] = '\0';
+        LcdString(stringed);
+
+        GotoXY(25, 5);
+        if(positionSettings == 3){
+            stringed[0] = ' ';
+            stringed[1] = ' ';
+            stringed[2] = ' ';
+            stringed[3] = '"';
+            stringed[4] = '\0';
+        }
+        if(positionSettings == 2){
+            stringed[0] = ' ';
+            stringed[1] = ' ';
+            stringed[2] = '"';
+            stringed[3] = ' ';
+            stringed[4] = '\0';
+        }
+        if(positionSettings == 1){
+            stringed[0] = ' ';
+            stringed[1] = '"';
+            stringed[2] = ' ';
+            stringed[3] = ' ';
+            stringed[4] = '\0';
+        }
+        if(positionSettings == 0){
+            stringed[0] = '"';
+            stringed[1] = ' ';
+            stringed[2] = ' ';
+            stringed[3] = ' ';
+            stringed[4] = '\0';
+        }
+        LcdString(stringed);
+    }
 }
 
 volatile bool b = 0;
@@ -120,9 +187,14 @@ void setup(){
     lastSpin = millis();
     lastButtonPress = millis();
     lastPrintLCD = millis();
+    EEPROM.write(0, 1);
+    EEPROM.write(1, 2);
+    EEPROM.write(2, 3);
+    EEPROM.write(3, 4);
     
     InitialiseTachometer();
     initializeValues();
+    normalMode = 1;
         
     pinMode(13, OUTPUT);
     digitalWrite(13, LOW);
@@ -131,22 +203,10 @@ void setup(){
     digitalWrite(9, LOW);
     pinMode(10, OUTPUT);
     digitalWrite(10, LOW);
+    
 }
 
 void loop(void){
-    /*GotoXY(0,0);
-    LcdString(" Km/h");
-    int val = EEPROM.read(0);
-    if(val == 3){
-        digitalWrite(9, HIGH);
-    }
-    EEPROM.write(0, 3);*/
-    /*GotoXY(0,0);
-    char stringed[11];
-    intToString(stringed, 7890); //v per day
-    LcdStringBig(stringed);
-    while(1){}*/
-    
     while(1){
         if(millis() - lastSpin > minSpeed){
             digitalWrite(9, HIGH);
@@ -203,46 +263,60 @@ void buttonsISR(){
       lastButtonPress = millis();
         digitalWrite(butt1, HIGH);
         if(digitalRead(buttonsPin) == HIGH){
-            digitalWrite(9, HIGH);
-            digitalWrite(10, LOW);
+            if(normalMode){
+               if(firstLeftPress + 500 < millis()){ //new row of presses
+                    firstLeftPress = millis();
+                    numberOfPressesLeft = 1;
+                }
+                else{
+                    numberOfPressesLeft++;
+                    firstLeftPress = millis();
+               }
+                if(numberOfPressesLeft > 4){
+                    normalMode = 0;
+                    numberOfPressesLeft = 0;
+                    //adjust wheel circumference
+                }
+            }
+            else{
+                if(positionSettings == 0){
+                    EEPROM.write(0, changeWheel[0]);
+                    EEPROM.write(1, changeWheel[1]);
+                    EEPROM.write(2, changeWheel[2]);
+                    EEPROM.write(3, changeWheel[3]);
+                    setWheelCircumferenceVariable();
+                    normalMode = 1;
+                    positionSettings = 3;
+                }
+                else{
+                    positionSettings--;
+                }
+            }
+            
         }
         else{
-            digitalWrite(10, HIGH);
-            digitalWrite(9, LOW);
+            if(normalMode){
+               if(firstRightPress + 500 < millis()){ //new row of presses
+                    firstRightPress = millis();
+                    numberOfPressesRight = 1;
+               }
+                 else{
+                   numberOfPressesRight++;
+                   firstRightPress = millis();
+               }
+               if(numberOfPressesRight > 4){ // delete stats
+                    initializeValues();
+               }
+            }
+            else{
+                if(changeWheel[positionSettings] < 9){
+                    changeWheel[positionSettings]++;
+                }
+                else{
+                    changeWheel[positionSettings] = 0;
+                }
+            }
         }
         digitalWrite(butt1, LOW);
     }
 }
-/*
-ISR(INT0_vect){
-    if(magneticSensorTickAllow == 1){
-        nullTimer(0);
-        magneticSensorTickAllow = 0;
-        needsToBeOne = 0;
-
-        if (underMinimalSpeed == 1){
-            underMinimalSpeed = 0;
-            nullTimer(1);
-        }
-        else{
-            numberOfRotations++;
-            t1 = t2;
-            t2 = t3;
-            t3 = readTimer(1);
-
-            nullTimer(1);
-            todayTimeInTimerPulses += t3;
-            #ifdef DEBUG
-            if(t3 == 0 || t3 < 3){
-                chyb++;
-            }
-            #endif
-        }
-    }
-    //todayTimeInTimerPulses += readTimer(1);
-    //nullTimer(1);
-}
-ISR(INT1_vect){
-    PORTB ^= 1<<PB1;
-    //todayTimeInTimerPulses = 0;
-}*/
